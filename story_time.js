da2a1cd340f6661b04b5e62957ce71ed52991649
Bertrand Moulard <bmoulard@etsy.com>
2015-08-28 14:46:13 -0400

    Take weekends into account. Change character to check mark. Deduce the tracker project ID from URL

--- story_time.js
+++ story_time.js
@@ -28,9 +28,20 @@ var updateStory = function(data, id) {
   for(var i = 0; i < data.length; i++) {
     if(data[i].highlight == "started") {
       var days = Math.ceil((Date.now() - Date.parse(data[i].occurred_at)) / 86400000);
-      updateStoryHtml(id, days);
+      updateStoryHtml(id, removeWeekends(days));
       return;
     }
+  }
+}
+
+var removeWeekends = function(days) {
+  var weekDay = (new Date()).getDay();
+  if(weekDay >= days) {
+    return days;
+  }
+  else {
+    days = days - (Math.floor(days/7) * 2);
+    return days;
   }
 }
 
@@ -47,7 +58,7 @@ var updateStoryHtml = function(id, days) {
 var indicatorText = function(days) {
   var text = "";
   for(var i = 0; i < days; i++) {
-    text += " ⦿";
+    text += " ✔︎";
   }
   return text + " ";
 };
@@ -56,21 +67,22 @@ var startTimer = function () {
   window.setTimeout(fetchStories, 30000);
 };
 
+var isTrackerProjectPage = function() {
+  return (window.location.href.indexOf("www.pivotaltracker.com/n/projects/") > -1);
+}
+
 var baseUrl = 'https://www.pivotaltracker.com/services/v5';
-var projectId = "1122726";
 
-var token = localStorage.getItem("api_token");
-if(!token || token == "null") {
-  token = prompt("Please enter your Pivotal Tracker API token");
-  localStorage.setItem("api_token", token);
-}
+if(isTrackerProjectPage()) {
+  var urlElements = window.location.href.split("/");
+  var projectId = urlElements[urlElements.length - 1];
+  var token = localStorage.getItem("api_token");
+  if(!token || token == "null") {
+    token = prompt("Please enter your Pivotal Tracker API token");
+    localStorage.setItem("api_token", token);
+  }
 
-var projectId = localStorage.getItem("project_id");
-if(!projectId || projectId == "null") {
-  projectId = prompt("Please enter your Pivotal Tracker project ID");
-  localStorage.setItem("project_id", projectId);
-}
-
-if(token && projectId && token != "null" && projectId != "null") {
-  window.setTimeout(fetchStories, 7000);
+  if(token && projectId && token != "null" && projectId != "null") {
+    window.setTimeout(fetchStories, 7000);
+  }
 }
